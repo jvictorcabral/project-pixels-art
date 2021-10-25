8eafddc910cfc9a440e370f089cfaa88ae6050b8
Joao <joaovictorcgjvcg@gmail.com>
2021-10-25 16:12:07 -0400

    Entrega requisito 5

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function pixelSquare() {
   for (let index = 0; index < 25; index++) {
     let div = document.createElement("div")
     div.className = "pixel"
-    div.id = index;
     let pixel = document.querySelector("#pixel-board")
     pixel.appendChild(div)
   }
