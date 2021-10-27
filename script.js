0077f11c4e80deeba14cae213aa25786db0b5e04
Joao <joaovictorcgjvcg@gmail.com>
2021-10-27 14:41:30 -0400

    Entrega Requisito 7

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-let numeroPixels = 5;
+const numeroPixels = 5;
 
 function pixelSquare() {
-  const pixel = document.querySelector("#pixel-board")
+  const pixel = document.querySelector('#pixel-board');
   for (let index = 0; index < numeroPixels; index += 1) {
-    const divLinha = document.createElement("div")
-    divLinha.className = "linha";
-    pixel.appendChild(divLinha)
+    const divLinha = document.createElement('div');
+    divLinha.className = 'linha';
+    pixel.appendChild(divLinha);
     for (let i = 0; i < numeroPixels; i += 1) {
-      const div = document.createElement("div")
-      div.className = "pixel"
-      divLinha.appendChild(div)
+      const div = document.createElement('div');
+      div.className = 'pixel';
+      divLinha.appendChild(div);
     }
   }
 }
 pixelSquare();
 
-function selected() {
-  const color = document.querySelector('.color')
-  color.addEventListener('click', cor)
+const section = document.getElementById('color-palette');
+function selected(event) {
+  const color = document.querySelector('.selected')
+if (color) {
+  color.classList.remove('selected')
 }
-
-function cor() {
-
+  event.target.classList.add('selected')
 }
+section.addEventListener('click', selected)
